2bf1ece878dfd402a9633a9ee965912c55837aee
Annie Gilbert <annie@tireagent.com>
2024-06-18 17:25:59 +0200

    follow Zeitwerk naming requirements

--- app/decorators/models/spree_signifyd/spree/order_decorator.rb
+++ app/decorators/models/spree_signifyd/spree/order_decorator.rb
@@ -1,11 +1,7 @@
-module SpreeSignifyd
-  module Spree
-    module OrderDecorator
-      def self.prepended(base)
-        base.include SpreeSignifyd::OrderConcerns
-      end
-
-      ::Spree::Order.prepend self
-    end
+module Models::SpreeSignifyd::Spree::OrderDecorator
+  def self.prepended(base)
+    base.include SpreeSignifyd::OrderConcerns
   end
+
+  ::Spree::Order.prepend self
 end
